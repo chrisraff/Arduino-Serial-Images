daaa1b14912e0cd5d746450ff1c206973f30c2fd
chrisraff <crharfifs@gmail.com>
2019-07-26 11:54:27 -0400

    Got rid of unecessary mid-image sleep comment

--- serial_frame_writer_demo.py
+++ serial_frame_writer_demo.py
@@ -28,13 +28,6 @@ while True:
     for i in range(64): # for each pixel
         data += bytes( [int( 127*sin(2*time()) + 127), 5, i*2] ) # the rgb values for this pixel
 
-        # the serial buffer can only hold 256 bytes at a time on ESP32
-        # if the image is really large, it may be necessary to send parts at a time
-        # but probably not since the arduino usually processes the data quicker than it is written
-        # if i % 21 == 0 or False:
-        #     ser.write(data)
-        #     data = b''
-        #     sleep(0.008)
     data += b']' # end the frame (arduino updates the display)
 
     # print(''.join([str(chr(c)) for c in data])) # print the string we sent
